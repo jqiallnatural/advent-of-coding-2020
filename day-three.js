6beef8c960ed5fbeff1dc757d26f0d5960b6d0aa
Jiaqi Zhao <jiaqi.zhao.tr@gmail.com>
2020-12-05 02:52:48 -0500

    Day 3 part 2

--- day-three.js
+++ day-three.js
@@ -19,3 +19,21 @@ const sortData = (splitString) => {
   return totalHash
 }
 console.log(sortData(splitString))
+
+// Part 2
+const multiplyDays = (splitString, newRight, newDown) => {
+  let totalHash = 0
+  let down = 0
+  let right = 0
+  for(down = 0; down < splitString.length; down += newDown){
+    let subArr = splitString[down]
+    let theHash = subArr[right % subArr.length]
+    if (theHash === '#') {
+      totalHash ++
+    }
+    right = right + newRight
+  }
+  return totalHash
+}
+
+console.log(multiplyDays(splitString, 1, 1) * multiplyDays(splitString, 3, 1) * multiplyDays(splitString, 5, 1) * multiplyDays(splitString, 7, 1) * multiplyDays(splitString, 1, 2))
